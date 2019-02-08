a00e004ad16793476c866668ac05ea5e8b1e06fd
centr <centrcompstroi@mail.ru>
2019-02-08 12:04:29 +0300

    2.2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once '../vendor/autoload.php';
 
 $road = new Road();
 $busMoving = new BusMoving();
-$transport = new RoadAnalizer($busMoving);
+$transport = new RoadAnalyzer($busMoving);
 echo $transport->calcMovingTime($road).'<br/>';
 
 $taxiMoving = new TaxiMoving();
